5e0c0407a86b4edec0685685e4f3f01b5d7fb38d
sourav kumar pandey <soukumpan@gmail.com>
2024-12-26 12:45:16 +0530

    minnor change

--- Working_with_http_request/locust_http_extract.py
+++ Working_with_http_request/locust_http_extract.py
@@ -3,12 +3,11 @@ from locust import HttpUser,task,between
 class MyUser(HttpUser):
     wait_time = between(1,2)
 
-    host = "https://www.geeksforgeeks.org/"
+    host = "https://www.javatpoint.com/"
 
     @task
     def login_URL(self):
-        res=self.client.get("/geeksforgeeks-practice-best-online-coding-platform/?ref=ghm",name="practice page")
+        res=self.client.get("simple-html-pages")
         print(res.text)
         print(res.status_code)
         print(res.headers)
-
